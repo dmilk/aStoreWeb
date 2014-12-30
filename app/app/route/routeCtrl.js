4757c7c3737c1f03fa0863a2cfb2e7bc386b4eb5
dmilk <dmitry_kul@mail.ru>
2014-12-30 10:30:31 +0300

    Update

--- app/app/route/routeCtrl.js
+++ app/app/route/routeCtrl.js
@@ -11,21 +11,28 @@ function config($routeProvider) {
                 controller: 'routeCtrl',
                 controllerAs: 'route'
             });
-};
+}
+;
 
 function routeCtrl(RouteService, CartService, TestService, authFactory) {
     var vm = this;
     vm.allRoutes = RouteService.query();
-    
-    vm.testClick = function() {
+
+    vm.testClick = function () {
 //        console.log("testClick");
         //vm.test = TestService.getSalt("Hello world!");
         authFactory.logout();
-        
+
     };
 
     vm.clickRoute = function (route) {
         CartService.routeId = route.id;
     };
-};
+
+    vm.clickRouteId = function (id) {
+        CartService.routeId = id;
+    };
+
+}
+;
 
